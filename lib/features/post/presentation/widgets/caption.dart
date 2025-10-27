11dc9a2ec51b0834af1b2c47fc72539144e06fd1
User <user@MacBook-Pro-cua-Loc.local>
2025-10-27 20:08:54 +0700

    custom widget ReadMoreText for package readmore

--- lib/features/post/presentation/widgets/caption.dart
+++ lib/features/post/presentation/widgets/caption.dart
@@ -1,3 +1,5 @@
+import 'package:app_instagram_clone/configs/logger/log.dart';
+import 'package:app_instagram_clone/cores/widgets/texts/read_more_text.dart';
 import 'package:flutter/material.dart';
 
 class Caption extends StatelessWidget {
@@ -8,31 +10,52 @@ class Caption extends StatelessWidget {
   final String _name;
   final String _caption;
 
+  InlineSpan _buildTextSpan(ColorScheme colorScheme) {
+    return TextSpan(
+      text: '$_name ',
+      style: TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w600,
+        letterSpacing: 0.13,
+        color: colorScheme.onSurface,
+      ),
+      children: [
+        TextSpan(
+          text: _caption,
+          style: const TextStyle(
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.26,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final ColorScheme colorScheme = themeData.colorScheme;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      child: RichText(
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-        text: TextSpan(
-          text: '$_name ',
-          style:  TextStyle(
-            fontSize: 13,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 0.13,
-            color: Theme.of(context).colorScheme.onSurface
-          ),
-          children: [
-            TextSpan(
-              text: _caption,
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.26,
-              ),
-            ),
-          ],
+      child: ReadMoreText(
+        preDataText: _name,
+        preDataTextStyle: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 0.13,
+          color: colorScheme.onSurface,
         ),
+        _caption,
+        style: const TextStyle(
+          fontWeight: FontWeight.w400,
+          letterSpacing: 0.26,
+        ),
+        trimMode: TrimMode.Line,
+        trimLines: 2,
+        textAlign: TextAlign.start,
+        trimCollapsedText: 'more',
+        trimExpandedText: '',
       ),
     );
   }
--- lib/features/post/presentation/widgets/post.dart
+++ lib/features/post/presentation/widgets/post.dart
@@ -14,6 +14,9 @@ import 'package:flutter/material.dart';
 class Post extends StatelessWidget {
   const Post({super.key});
 
+  static const String lorem =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
@@ -38,11 +41,7 @@ class Post extends StatelessWidget {
           countRepost: 1152,
           countShare: 7,
         ),
-        const Caption(
-          name: 'Dinh Xuan Loc',
-          caption:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        ),
+        const Caption(name: 'Dinh Xuan Loc', caption: lorem),
       ],
     );
   }
